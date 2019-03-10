dc25c3dc7659ef92b576e06a423a2f5e0465633e
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-10 22:01:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,46 +1,49 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-  input.to_i - 1
-end
-
-
-def move(board, input, value = "X")
-  board[input] = value
-end
-
-
-def valid_move?(board, index)
-  def position_taken?(array, user)
-    if array[user] == " " || array[user] == "" || array[user] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8)
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-end
-
 def turn(board)
-  puts "Please enter 1-9"
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+
+  def move(board, input, value = "X")
+    board[input] = value
+  end
+
+
+  def valid_move?(board, index)
+    def position_taken?(array, user)
+      if array[user] == " " || array[user] == "" || array[user] == nil
+        return false
+      else
+        return true
+      end
+    end
+
+    def on_board?(num)
+      if num.between?(0, 8)
+        return true
+      else
+        return false
+      end
+    end
+
+    if (position_taken?(board, index)) == false && (on_board?(index) == true)
+      return true
+    else
+      return false
+    end
+  end
+
+  counter = 0
+    while counter < 9
+      counter += 1
+      puts(value)
+    end
 end
